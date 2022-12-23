90ab263b4912587497d7b4a40eec2a3b056129ae
karthikd123 <karthikd@brightcomgroup.com>
2022-12-23 12:34:24 +0000

    T7

---
+++ python-hello_world/2-positive_or_negative.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+#include <stdlib.h>
+#include <time.h>
+#include <studio.h>
+/**
+  *main- prints if number is positive, zero or negetive
+  *Return: Always (Success)
+  *
+*/
+int main(void)
+
+{	int n;
+	srand (time(0));
+	n= rand() - RAND_MAX / 2;
+ 	if (n>0)
+	{ printf("%d is positive\n", n);
+	}
+	else if (n== 0)
+	{ printf("%d is zero\n", n);
+	}
+	else{ printf("%d is negetive\n", n);
+	}
+	return (0);
+}
